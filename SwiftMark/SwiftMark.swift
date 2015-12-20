1d4d7cf6f167340aa790e74bb1259fc740ef0122
Pyroh <pyroh@icloud.com>
2015-12-20 12:55:55 +0100

    [ADD] SMarkConvertToHTMLOperation

--- SwiftMark/SwiftMark.swift
+++ SwiftMark/SwiftMark.swift
@@ -9,15 +9,18 @@
 import Foundation
 import libcmark
 
+public typealias ConvertionCompletionBlock = (String) -> ()
+public typealias FailureBlock = () -> ()
+
 /**
  Convert `str` to HTML, with `options` passed to the parser.
  */
-public func markdownToHTML(str: String, options: SwiftMarkOptions) -> String {
+public func markdownToHTML(str: String, options: SwiftMarkOptions) -> String? {
     var output: String?
     str.withCString {
         let buf = cmark_markdown_to_html($0, Int(strlen($0)), options.rawValue)
         output = String(CString: buf, encoding: NSUTF8StringEncoding)
         free(buf)
     }
-    return output ?? ""
+    return output
 }
