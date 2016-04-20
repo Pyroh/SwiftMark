543ef251651743cad47d7466e15229c370e9b2d4
Pyroh <pyroh@icloud.com>
2016-04-20 11:35:24 +0200

    [ADD] Package.swift
    [FIX] doc generation

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  CoreGeometry
+//
+//  Created by Pierre TACCHI on 20/04/16.
+//  Copyright © 2016 Pyrolyse. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftMark"
+)
